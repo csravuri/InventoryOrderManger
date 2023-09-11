d65e7a28a218c9b04b92ecce76550dc552567fbb
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-09-11 22:06:53 +0530

    home page layout and bindings

--- IOManager/ViewModels/HomeViewModel.cs
+++ IOManager/ViewModels/HomeViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace IOManager.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace IOManager.ViewModels
 {
-	public class HomeViewModel
+	public partial class HomeViewModel : ObservableObject
 	{
+		[RelayCommand]
+		void ItemCreate()
+		{
+		}
+
+		[RelayCommand]
+		void ItemView()
+		{
+		}
+
+		[RelayCommand]
+		void OrderCreate()
+		{
+		}
+
+		[RelayCommand]
+		void OrderView()
+		{
+		}
+
+		[RelayCommand]
+		void Backup()
+		{
+		}
+
+		[RelayCommand]
+		void Restore()
+		{
+		}
 	}
 }
