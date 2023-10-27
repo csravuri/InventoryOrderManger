08079e154a6b9a98fb77b5fb75ef3a555c30b2db
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-10-27 17:50:36 +0530

    order create page basic details

--- IOManager/ViewModels/OrderCreateEditViewModel.cs
+++ IOManager/ViewModels/OrderCreateEditViewModel.cs
@@ -1,8 +1,51 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IOManager.Database;
 
 namespace IOManager.ViewModels
 {
 	public partial class OrderCreateEditViewModel : ObservableObject
 	{
+		public OrderCreateEditViewModel(DbConnection connection)
+		{
+			Connection = connection;
+		}
+
+		[ObservableProperty]
+		string customerName;
+
+		[ObservableProperty]
+		bool isWholeSale = true;
+
+		[RelayCommand]
+		async Task AddItems()
+		{
+		}
+
+		[RelayCommand]
+		void IsWholeSaleTapped()
+		{
+			IsWholeSale = !IsWholeSale;
+		}
+
+		[RelayCommand]
+		async Task Back()
+		{
+			await Shell.Current.GoToAsync("..");
+		}
+
+		[RelayCommand]
+		void Clear()
+		{
+			CustomerName = null;
+			IsWholeSale = true;
+		}
+
+		[RelayCommand]
+		async Task Save()
+		{
+		}
+
+		DbConnection Connection { get; }
 	}
 }
