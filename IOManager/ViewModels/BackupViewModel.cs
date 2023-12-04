abf8d898a75de3ab2598495d44f824e143d36845
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-12-04 17:55:36 +0530

    local share option added

--- IOManager/ViewModels/BackupViewModel.cs
+++ IOManager/ViewModels/BackupViewModel.cs
@@ -1,8 +1,76 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IOManager.Database;
+using IOManager.Utils;
 
 namespace IOManager.ViewModels
 {
 	public partial class BackupViewModel : ObservableObject
 	{
+		public BackupViewModel(DbConnection connection)
+		{
+			Connection = connection;
+		}
+
+		[RelayCommand]
+		async Task Create()
+		{
+			try
+			{
+				Status = "Please wait, we are creating backup";
+				var helper = new FileTransferHelper(Connection);
+				ZipFilePath = await helper.GetBackupZip();
+				Status = "File Created, Ready to share";
+			}
+			catch (Exception ex)
+			{
+				Status = ex.ToString();
+			}
+
+		}
+
+		[RelayCommand]
+		void LocalShare()
+		{
+			if (!File.Exists(ZipFilePath))
+			{
+				Shell.Current.DisplayAlert("Error!", "Create backup before share", "Ok");
+				return;
+			}
+
+			Share.Default.RequestAsync(new ShareFileRequest
+			{
+				File = new ShareFile(ZipFilePath),
+				Title = "Share or save file"
+			});
+		}
+
+		[RelayCommand]
+		void CloudShare()
+		{
+		}
+
+		[RelayCommand]
+		void Done()
+		{
+			Status = string.Empty;
+			ZipFilePath = string.Empty;
+		}
+
+		[ObservableProperty]
+		string status;
+
+		[ObservableProperty]
+		string fileName;
+
+		[ObservableProperty]
+		string zipFilePath;
+
+		partial void OnZipFilePathChanged(string value)
+		{
+			FileName = string.IsNullOrWhiteSpace(value) ? string.Empty : Path.GetFileName(ZipFilePath);
+		}
+
+		public DbConnection Connection { get; }
 	}
 }
